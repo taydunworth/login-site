13c8274c92389d71f93c1441d09dd480b3159fd5
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-27 21:36:55 -0400

    Added login functionality with redirects

--- main.js
+++ main.js
@@ -2,32 +2,36 @@ const express = require('express')
 const path = require('path')
 const mustacheExpress = require('mustache-express')
 const expressSession = require('express-session')
-
+const bodyParser = require('body-parser')
 const app = express()
 
-app.use(express.static('public'))
 app.engine('mustache', mustacheExpress())
-
 app.set('views', './views')
 app.set('view engine', 'mustache')
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
+const loginInfo = (req, res, next) => {
+  if (req.body.username === 'taylorcox' && req.body.password === 'passstuff') {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
 
-app.get('/', function(req, res) {
-  res.render('home')
-})
-
-app.post('/logged-in', (req, res) => {
-  res.render('logged-in')
+app.get('/login', (req, res) => {
+  res.render('login')
 })
 
 app.post('/', (req, res) => {
-  res.render('logged-in')
+  res.render('home', req.body)
+})
+
+app.use(loginInfo)
+
+app.post('/logged-in', (req, res) => {
+  res.render('logged-in', req.body)
 })
 
 app.listen(3000, function() {
